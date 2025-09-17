28e590bf7e322ec0f66030af529d1f8a5d2e73a9
Kiran272003 <b.kiran@encora.com>
2025-09-17 15:23:22 +0530

    Integration testing

--- src/main/java/com/example/demo/Service/BlogsService.java
+++ src/main/java/com/example/demo/Service/BlogsService.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-//import com.example.demo.Exceptions.BadException;
-//import com.example.demo.Exceptions.ResourceNotFoundException;
+import com.example.demo.Exceptions.BadException;
+import com.example.demo.Exceptions.ResourceNotFoundException;
 import com.example.demo.Repository.*;
 
 @Service
@@ -23,28 +23,28 @@ public class BlogsService {
 	
 	public Blogs addBlog(Blogs blog)
 	{
-//		 if (blog.getName() == null || blog.getName().isEmpty()) {
-//		        throw new BadException("Creator name cannot be null or empty.");
-//		    }
+		 if (blog.getName() == null || blog.getName().isEmpty()) {
+		        throw new BadException("Creator name cannot be null or empty.");
+		    }
 		return blogrepository.save(blog);
 	}
 	
 	public Optional<Blogs> getBlogById(Long Id)
 	{
 		Optional<Blogs> blog = blogrepository.findById(Id);
-//		if(blog.isEmpty())
-//		{
-//			throw new ResourceNotFoundException("Blog not found with ID: " + Id);
-//		}
+		if(blog.isEmpty())
+		{
+			throw new ResourceNotFoundException("Blog not found with ID: " + Id);
+		}
 		return blog;
 	}
 	
 	public void deleteBlogsById(Long Id) {
 		Optional<Blogs> blog = blogrepository.findById(Id);
-//		if(blog.isEmpty())
-//		{
-//			throw new ResourceNotFoundException("Blog not found with ID: " + Id);
-//		}
+		if(blog.isEmpty())
+		{
+			throw new ResourceNotFoundException("Blog not found with ID: " + Id);
+		}
 	  blogrepository.deleteById(Id);
 	}
 	
